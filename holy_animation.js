27a0e26ad70d6af52ce02fad772f08380db5e77a
kaioneill <oneillkai@icloud.com>
2018-11-11 18:19:17 -0800

    more animation

--- holy_animation.js
+++ holy_animation.js
@@ -4,5 +4,9 @@ window.onload = function() {
   thing.onclick = () => {
     TweenLite.to(thing, 1, {left: '+=100px'});
   }
+  var thing1 = document.querySelector('#thing1');
+  thing1.onclick = () => {
+    TweenLite.to(thing1, 1, {height: '+=20px', width: '+=20px'});
+  }
 
 }
