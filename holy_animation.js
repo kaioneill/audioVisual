ca0c7a98ea165d59cdb4a5c0dd7d8a725b678ad0
kaioneill <oneillkai@icloud.com>
2018-11-11 18:57:32 -0800

    keyboard esque

--- holy_animation.js
+++ holy_animation.js
@@ -1,12 +1,32 @@
 window.onload = function() {
 
-  var thing = document.querySelector('#thing');
-  thing.onclick = () => {
-    TweenLite.to(thing, 1, {left: '+=100px'});
-  }
-  var thing1 = document.querySelector('#thing1');
-  thing1.onclick = () => {
-    TweenLite.to(thing1, 1, {height: '+=20px', width: '+=20px'});
+  var squares = document.querySelectorAll('.square');
+  console.log(squares);
+
+  for (let s of squares) {
+    s.style.backgroundColor = Math.floor(Math.random()*16777215).toString(16);
+    s.onclick = s.onclick = () => {
+      clickBounce(s);
+    }
   }
 
+
+
+
+  // var thing = document.querySelector('#thing');
+  // thing.onclick = () => {
+  //   clickBounce(thing);
+  // }
+  // var thing1 = document.querySelector('#thing1');
+  // thing1.onclick = () => {
+  //   clickBounce(thing1);
+  // }
+
+}
+
+
+clickBounce = function(clickedItem) {
+  var tl = new TimelineLite();
+  tl.to(clickedItem, .1, {height: '+=10px', width: '+=10px'})
+  .to(clickedItem, .2, {height: '-=10px', width: '-=10px'});
 }
