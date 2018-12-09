a9e9e32300208d323de77cc52973ccfbdfd6e5bf
kaioneill <oneillkai@icloud.com>
2018-12-08 17:51:08 -0800

    added colume slider and detune

--- holy_animation.js
+++ holy_animation.js
@@ -11,12 +11,16 @@ window.onload = function() {
     template: '<div class="pad"></div>'
   })
 
+  Vue.component('slider', {
+    template: '<input type="range" min="0" max="100" value="50" class="slider">'
+  })
+
   var app = new Vue({
     el: '#app',
     data: {
       mainName: 'sound_mkr',
 
-      globalVolume: .5,
+      globalVolume: 0.5,
       gainNode: null,
       delayNode: [],
       audioCtx: null,
@@ -24,11 +28,11 @@ window.onload = function() {
       keyMap: {
         'a': 'C4',
         's': 'D4',
-        'd': 'E4',
+        'd': 'Eb4',
         'f': 'F4',
         'g': 'G4',
-        'h': 'A4',
-        'j': 'B4',
+        'h': 'Ab4',
+        'j': 'Bb4',
         'k': 'C5',
       },
       pressedKeys: [],
@@ -47,14 +51,18 @@ window.onload = function() {
         {id: 0, name: 'volume'},
         {id: 1, name: 'filter'},
       ],
+      sliders: [
+        {id: 0, name: 'volume'},
+        {id: 1, name: 'filter'},
+      ],
       pads: [
         {id: 0, name: 'C4', freq: '261.63'},
         {id: 1, name: 'D4', freq: '293.66'},
-        {id: 2, name: 'E4', freq: '329.63'},
+        {id: 2, name: 'Eb4', freq: '311.13'},
         {id: 3, name: 'F4', freq: '349.23'},
         {id: 4, name: 'G4', freq: '392.00'},
-        {id: 5, name: 'A4', freq: '440.00'},
-        {id: 6, name: 'B4', freq: '493.88'},
+        {id: 5, name: 'Ab4', freq: '415.30'},
+        {id: 6, name: 'Bb4', freq: '466.16'},
         {id: 7, name: 'C5', freq: '523.25'},
       ]
     },
@@ -104,9 +112,16 @@ window.onload = function() {
 
           if(self.holdFlag[pressed]) {
             if(!self.oscs[pressed]) {
-              self.oscs[pressed] = app.audioCtx.createOscillator()
-              self.oscs[pressed].type = 'sawtooth'
-              self.oscs[pressed].detune.value = 12
+              self.oscs[pressed] = []
+              self.oscs[pressed].push(app.audioCtx.createOscillator())
+              self.oscs[pressed][0].type = 'sawtooth'
+              self.oscs[pressed][0].detune.value = 0
+              self.oscs[pressed].push(app.audioCtx.createOscillator())
+              self.oscs[pressed][1].type = 'sawtooth'
+              self.oscs[pressed][1].detune.value = 15
+              self.oscs[pressed][0].frequency.value = parseFloat(p.getAttribute('freq'))
+              self.oscs[pressed][1].frequency.value = parseFloat(p.getAttribute('freq'))
+
 
               self.gains[pressed] = app.audioCtx.createGain()
               self.gains[pressed].gain.value = 0;
@@ -116,11 +131,14 @@ window.onload = function() {
               self.filters[pressed].frequency.value = app.filterFreq;
               self.filters[event.key].frequency.linearRampToValueAtTime(0, audioCtx.currentTime + app.filterDecay + app.ampAttack);
 
-              self.oscs[pressed].frequency.value = parseFloat(p.getAttribute('freq'))
-              self.oscs[pressed].connect(self.gains[pressed])
+
+
+              self.oscs[pressed][0].connect(self.gains[pressed])
+              self.oscs[pressed][1].connect(self.gains[pressed])
               self.gains[pressed].connect(self.filters[pressed])
               self.filters[pressed].connect(sendTo)
-              self.oscs[pressed].start(0)
+              self.oscs[pressed][0].start(0)
+              self.oscs[pressed][1].start(0)
 
               TweenLite.to(p, .1, {height: '60px', backgroundColor: '#9900cc'})
               self.holdFlag[pressed] = false
@@ -129,7 +147,8 @@ window.onload = function() {
               if(self.keyMap.hasOwnProperty(event.key)) {
                 p = document.querySelector('#pad-' + self.keyMap[event.key])
                 self.gains[event.key].gain.linearRampToValueAtTime(0, audioCtx.currentTime + app.ampRelease + app.ampAttack);
-                self.oscs[event.key].stop(app.audioCtx.currentTime + .51)
+                self.oscs[event.key][0].stop(app.audioCtx.currentTime + .51)
+                self.oscs[event.key][1].stop(app.audioCtx.currentTime + .51)
                 self.oscs[event.key] = null
                 TweenLite.to(p, .2, {height: '50px', backgroundColor: '#cc0066'});
                 self.holdFlag[event.key] = true
@@ -138,6 +157,24 @@ window.onload = function() {
             }
           }
         });
+      },
+      sliderMove: function(slider) {
+
+        sliderElement = document.querySelector('#' + slider.name)
+
+        // var holdFlag = false;
+
+        document.onmousemove = (e) => {
+
+          if(slider.name == 'volume') {
+            app.globalVolume = sliderElement.value/100
+            app.gainNode.gain.linearRampToValueAtTime(app.globalVolume, .01);
+            console.log(app.globalVolume);
+          }
+
+        }
+
+
       }
     },
     mounted() {
@@ -151,13 +188,13 @@ window.onload = function() {
       var gainNode = audioCtx.createGain();
 
       self.gainNode = gainNode;
-      self.gainNode.gain.setValueAtTime(1, self.audioCtx.currentTime);
+      self.gainNode.gain.setValueAtTime(.5, self.audioCtx.currentTime);
       self.gainNode.connect(self.audioCtx.destination);
 
       var delayGainNode = audioCtx.createGain();
 
 
-      delayGainNode.gain.setValueAtTime(1, self.audioCtx.currentTime);
+      delayGainNode.gain.setValueAtTime(.1, self.audioCtx.currentTime);
       delayGainNode.connect(self.audioCtx.destination);
 
       var delayNode = [];
@@ -177,63 +214,9 @@ window.onload = function() {
 
       var sendTo = self.gainNode;
 
-
-
       window.addEventListener('keydown', function(e) {
         self.keyPress(e, self.gainNode, self.audioCtx); // declared in your component methods
       });
     }
   })
-
-
-
-
-  //
-  // for(var i=0;i<4;i++) {
-  //   delayNode.push(audioCtx.createDelay());
-  //   delayNode[i].delayTime.setValueAtTime(parseFloat(.3*i), audioCtx.currentTime);
-  //   delayNode[i].connect(audioCtx.destination);
-  // }
-  //
-  //
-  // for(var i=0;i<delayNode.length;i++) {
-  //   gainNode.gain.setValueAtTime((1-((i*2)/10))-i/10, audioCtx.currentTime);
-  //   gainNode.connect(delayNode[i]);
-  // }
-
-
-
-  // var knobs = document.querySelectorAll('.knob');
-  // for (let k of knobs) {
-  //   knobAction(k);
-  // }
 }
-
-
-// knobAction = function(k) {
-//   var holdFlag = false;
-//   k.onmousedown = (e) => {
-//     var oldY;
-//     e.preventDefault();
-//     holdFlag = true;
-//     document.onmousemove = (e) => {
-//       if(holdFlag) {
-//         oldY = e.pageY;
-//
-//
-//         setTimeout( function() {
-//           if(k.id == 'volume') globalVolume += (e.pageY-oldY)/400;
-//           TweenLite.to(k, .1, {rotation: "+=" + ((e.pageY-oldY)).toString()});
-//         }, 100);
-//
-//         if(k.id == 'volume') {
-//           gainNode.gain.setValueAtTime(globalVolume, audioCtx.currentTime);
-//           console.log(globalVolume);
-//         }
-//       }
-//     }
-//     window.onmouseup = () => {
-//       holdFlag = false;
-//     }
-//   }
-// }
